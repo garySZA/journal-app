8820586969ba8203cdaeb07a74160611e5ed2f9e
GarySZ <garysoliz07@gmail.com>
2024-04-03 11:23:38 -0400

    add - .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,17 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore/lite';
+
+import config from '../config/variables';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBZ-xEtDA-494dmTMpfRe51wxMJkARAyIE",
-    authDomain: "react-journal-app-28445.firebaseapp.com",
-    projectId: "react-journal-app-28445",
-    storageBucket: "react-journal-app-28445.appspot.com",
-    messagingSenderId: "959255334917",
-    appId: "1:959255334917:web:0312995a65bceedf1e8c6c"
+    apiKey: config.apiKey,
+    authDomain: config.authDomain,
+    projectId: config.projectId,
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId,
+    appId: config.appId
 };
 
 // Initialize Firebase
